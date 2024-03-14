b53e3823eac1ce5470bed5853bf3ec5e811b5e97
iamabdulali <abdulqayyum354@gmail.com>
2024-03-14 22:14:21 +0500

    commit

--- server/public/script.js
+++ server/public/script.js
@@ -645,7 +645,105 @@ $("#shirtSize").click(function (params) {
   $("#edit-btn").show();
   $("#cart-btn").show();
   $(".shirt-size-menu").show();
+  $(".size-guide-div").show();
+  $("#size-guide-btn").show();
 });
+
+$(".size-guide").hide();
+
+$("#size-guide-btn").click(function (params) {
+  $(".overlay").show();
+  $(".size-guide").show();
+});
+
+$(".overlay").click(function (params) {
+  $(".overlay").hide();
+  $(".size-guide").hide();
+});
+
+setTimeout(() => {
+  $("#inch").addClass("selected-guide");
+
+  $("#inch").click(function (params) {
+    mostrarDatos1();
+  });
+  $("#centimeter").click(function (params) {
+    mostrarDatos2();
+  });
+  $(document).on("click", ".measure-btn", function (params) {
+    $(".measure-btn").removeClass("selected-guide");
+    $(this).addClass("selected-guide");
+  });
+  // $(".measure-btn").click(function (params) {});
+
+  $(".close-menu").click(function (params) {
+    $(".overlay").hide();
+    $(".size-guide").hide();
+  });
+}, 1000);
+
+function mostrarDatos2() {
+  var datos = [
+    { a: "S", b: 45.7, c: 71.1, d: 20.9 },
+    { a: "M", b: 50.8, c: 73.6, d: 21.6 },
+    { a: "L", b: 55.9, c: 76.2, d: 22.2 },
+    { a: "XL", b: 60.1, c: 78.7, d: 22.9 },
+    { a: "XXL", b: 66.1, c: 81.3, d: 23.5 },
+    { a: "3XL", b: 71.1, c: 83.9, d: 24.1 },
+  ];
+
+  actualizarTabla(datos, "encabezado2");
+  // Resaltar el botón seleccionado
+  document.getElementById("inch").classList.remove("active");
+  document.getElementById("centimeter").classList.add("active");
+}
+
+// Función para mostrar datos 1
+function mostrarDatos1() {
+  var datos = [
+    { a: "S", b: 18, c: 28, d: 8.2 },
+    { a: "M", b: 20, c: 29, d: 8.5 },
+    { a: "L", b: 22, c: 30, d: 8.7 },
+    { a: "XL", b: 24, c: 31, d: 9.1 },
+    { a: "XXL", b: 26, c: 32, d: 9.2 },
+    { a: "3XL", b: 28, c: 33, d: 9.5 },
+  ];
+
+  actualizarTabla(datos, "encabezado1");
+  // Resaltar el botón seleccionado
+  document.getElementById("inch").classList.add("active");
+  document.getElementById("centimeter").classList.remove("active");
+}
+
+// Función para actualizar la tabla con nuevos datos y encabezados
+function actualizarTabla(datos, encabezadoId) {
+  // Ocultar todos los encabezados
+  document.getElementById("encabezado1").style.display = "none";
+  document.getElementById("encabezado2").style.display = "none";
+
+  // Mostrar el encabezado correspondiente al conjunto de datos
+  document.getElementById(encabezadoId).style.display = "table-header-group";
+
+  var cuerpoTabla = document.getElementById("cuerpoTabla");
+  cuerpoTabla.innerHTML = "";
+
+  for (var i = 0; i < datos.length; i++) {
+    var fila = cuerpoTabla.insertRow(i);
+    var celdaa = fila.insertCell(0);
+    var celdab = fila.insertCell(1);
+    var celdac = fila.insertCell(2);
+    var celdad = fila.insertCell(3);
+
+    celdaa.innerHTML = datos[i].a;
+    celdab.innerHTML = datos[i].b;
+    celdac.innerHTML = datos[i].c;
+    celdad.innerHTML = datos[i].d;
+
+    celdaa.innerHTML = "<strong>" + datos[i].a + "</strong>";
+    celdab.innerHTML = datos[i].b;
+    celdac.innerHTML = datos[i].c;
+  }
+}
 
 $("#edit-btn").click(function (params) {
   $(this).hide();
@@ -655,6 +753,7 @@ $("#edit-btn").click(function (params) {
   $("#cart-btn").hide();
   $(".shirt-size-menu").hide();
   $(".select-shirt-color-div").hide();
+  $(".size-guide-div").hide();
 });
 
 $(".menu-form").submit(function (e) {
